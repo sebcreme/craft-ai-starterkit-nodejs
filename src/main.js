11e0df9414279f8a4baade1f8625caf516f74ae0
Clodéric Mars <cloderic.mars@craft.ai>
2015-11-26 19:42:54 +0100

    Complete hello world

--- src/main.js
+++ src/main.js
@@ -1,14 +1,34 @@
+import createInstance from 'craft-ai-client-js';
 import loadCfg from './loadCfg';
-import * as craft from 'craft-ai-client-js';
+import ON_DEATH from 'death';
 
 loadCfg()
-  .then((config) => craft.createInstance(
-    config.owner,
-    config.name,
-    config.version,
-    config.appId,
-    config.appSecret))
-  .then(() => craft.createAgent('bts/hello_world.bt', {}))
-  .then(() => craft.update())
-  .then(() => craft.destroyInstance())
-  .catch((err) => console.log('Unexpected error!', err))
+  .then(config => createInstance(config))
+  .then(instance => {
+    console.log(`'${instance.instanceId}' successfully created!`);
+    ON_DEATH(() => {
+      instance.destroy()
+        .then(() => console.log(`'${instance.instanceId}' successfully destroyed!`));
+    });
+    return instance.createAgent('bts/hello_world.bt', {name: 'juliette'})
+      .then(agent => console.log(`agent #${agent.id} created.`))
+      .then(() => instance.createAgent('bts/hello_world.bt', {name: 'romeo'}))
+      .then(agent => console.log(`agent #${agent.id} created.`))
+      .then(() => instance.registerAction(
+        'HelloWorld',
+        (requestId, agentId, input, success, failure) => {
+          console.log(`#${agentId}: "Hello World!"`);
+          success();
+        }))
+      .then(() => instance.registerAction(
+        'SayMyName',
+        (requestId, agentId, input, success, failure) => {
+          console.log(`#${agentId}: "My name is ${input.name}."`);
+          success();
+        }))
+      .then(() => instance.update(500));
+  })
+  .catch((err) => {
+    console.log('Unexpected error!');
+    console.log(err);
+  });
